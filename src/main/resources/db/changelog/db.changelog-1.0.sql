09d60fa0c580d272dab304793b8480a1e86bb459
AleksandrKamen <Kamen.alexandr@mail.ru>
2024-04-12 21:00:08 +0300

    add: index for userName

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -4,10 +4,12 @@
 CREATE TABLE IF NOT EXISTS users
 (
   id BIGSERIAL PRIMARY KEY,
-  username VARCHAR(64) NOT NULL UNIQUE,
+  username VARCHAR(64) NOT NULL UNIQUE ,
   password VARCHAR(128) NOT NULL,
   firstname VARCHAR(64),
   lastname VARCHAR(64),
   role VARCHAR(32),
   signup_method VARCHAR(32)
 );
+--changeset kamenev:2
+create index idx_userName on users(username);
